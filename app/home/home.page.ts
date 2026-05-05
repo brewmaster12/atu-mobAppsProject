20ae61c92f7a9ace8b96957f5bea4a5f55c4de03
brewmaster12 <seanywg@gmail.com>
2026-05-05 15:06:32 +0100

    implement "trending movies" feature

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard } from '@ionic/angular/standalone';
+import { MoviesService } from '../services/movies-service';
+import { HttpOptions } from '@capacitor/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  trendingMovies: any;
+
+  options: HttpOptions = {
+    url: "https://api.themoviedb.org/3/trending/movie/day?api_key=2bef00b6e8260141111bda3da66d2688"
+  }
+
+  constructor(private mhs:MoviesService) {}
+
+  ngOnInit() {
+    this.trendingMovies = [];
+    this.getMovies();
+  }
+
+  async getMovies() {
+    let result = await this.mhs.get(this.options);
+    console.log(result.data.results[1].title);
+    console.log(result.data.results); // ARRAY of trending movies
+    this.trendingMovies = result.data.results;
+
+  }
+
 }
