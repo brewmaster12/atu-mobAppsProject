7deddcecea8636c9dc54fac9893af23263467600
brewmaster12 <seanywg@gmail.com>
2026-05-05 16:13:38 +0100

    add search button

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard, IonInput, IonButton } from '@ionic/angular/standalone';
 import { MoviesService } from '../services/movies-service';
 import { HttpOptions } from '@capacitor/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonCard, CommonModule, IonInput, FormsModule, IonButton],
 })
 export class HomePage {
   trendingMovies: any;
+  keyword: string = "";
 
   options: HttpOptions = {
     url: "https://api.themoviedb.org/3/trending/movie/day?api_key=2bef00b6e8260141111bda3da66d2688"
