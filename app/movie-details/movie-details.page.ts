2d7d693d774098cf1aed91e7ffb0bdae11d377cf
brewmaster12 <seanywg@gmail.com>
2026-05-06 20:38:58 +0100

    add ion cards including images to movie details page

--- app/movie-details/movie-details.page.ts
+++ app/movie-details/movie-details.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { Data } from '../services/data';
 import { MoviesService } from '../services/movies-service';
 import { HttpOptions } from '@capacitor/core';
@@ -11,7 +11,7 @@ import { HttpOptions } from '@capacitor/core';
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle]
 })
 export class MovieDetailsPage implements OnInit {
 
@@ -40,6 +40,10 @@ export class MovieDetailsPage implements OnInit {
     const result = await this.mhs.get(movieOptions);
     this.movie = result.data
     console.log(this.movie)
+  }
+
+  openDetails() {
+    console.log("Opening details page ...");
   }
 
 }
